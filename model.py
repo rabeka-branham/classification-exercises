bd2c0d29279194c048d8446760ec60dcfc8c35d3
Rabeka Branham <rabeka.june@gmail.com>
2023-12-06 16:52:08 -0600

    uploading at end-of-day

--- model.py
+++ model.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def preprocess_titanic(train_df, validate_df, test_df):
     encoded_df = []
     for df in [train_df, validate_df, test_df]:
@@ -6,4 +8,21 @@ def preprocess_titanic(train_df, validate_df, test_df):
               drop_first=True).astype(int)
         encoded_df.append(pd.concat([df, df_encoded_columns],
                                     axis=1).drop(columns=['sex', 'embark_town']))
+    return encoded_df
+
+def preprocess_telco(train, validate, test):
+    encoded_df = []
+    
+    for df in [train, validate, test]:
+        yes_no_columns = ['multiple_lines', 'online_security', 'online_backup', 'device_protection', 'tech_support', 'streaming_tv', 'streaming_movies']
+
+        for col in yes_no_columns:
+            df[col] = np.where(df[col] == 'Yes', 'Yes', 'No')
+            
+        encoded_columns = df.columns[df.nunique() < 10].tolist()
+        
+        df_encoded_columns = pd.get_dummies(df[encoded_columns], drop_first=True).astype(int)
+        df = pd.concat([df, df_encoded_columns], axis=1).drop(columns=encoded_columns)
+        encoded_df.append(df)
+    
     return encoded_df
